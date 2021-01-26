058fdb59c5568e55d48deabfabb8fd46cf5f9946
RyanGlem <ryanglem@hotmail.com>
2021-01-26 17:50:51 -0500

    Routed Frontedn to Backend

---
+++ recipe-app/recipe-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import HomeDisplay, {  } from './components/RecipeHome';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeDisplay/>
+    </div>
+  );
+}
+
+export default App;
