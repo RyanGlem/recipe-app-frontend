8647d235a4178d35e0b0399b7f940b34898ca510
RyanGlem <60987736+RyanGlem@users.noreply.github.com>
2021-01-27 10:25:14 -0500

    Delete giphy-search directory

--- giphy-search/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import GifData from './components/gifData'
-import GifSearch from './components/gifSearch'
-
-function App() {
-  return (<div><GifSearch /></div>);
-}
-
-export default App;
